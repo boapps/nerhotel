778a736eb364a2d254a6b6064d2e9e74afdd9d7b
boa <boa@protonmail.com>
2024-07-01 14:55:31 +0200

    Updates

---
+++ dist/assets/about-f13bbf21.js
@@ -0,0 +1,49 @@
+const e={},t=`<section>
+<h1>Mi az a NER Hotel?</h1>
+<p>A magyar közéletet régóta jellemzi a politikai elit törekvése egy vele összefonódó vállalkozói réteg létrehozására. Megfelelő kapcsolatokkal és politikai hátszéllel akár néhány év alatt
+jelentős vagyont lehet felhalmozni célzott közbeszerzések, vissza nem térítendő támogatások és kedvezményes hitelek révén. Mára a vendéglátás és az idegenforgalom területén is
+megkerülhetetlenné váltak azok a vállalkozások, amelyek ennek a folyamatnak a haszonélvezői, hotelek és éttermek százai kerültek politikaközeli szereplők érdekeltségébe szerte az
+országban. A rendszerszerű klientúraépítés felett az állampolgári kontroll lehetőségei beszűkültek, vásárlóként azonban továbbra is lehetőségünk van a pénztárcánkkal szavazni a vállalkozók
+mellett és a politikai hátszéllel segített vállalkozói réteg további térhódítása ellen.</p>
+<p><strong>A NER HOTEL egy olyan webes alkalmazás, ami átláthatóbbá teszi a magyar szállás- és a vendéglátóhelyek tulajdonosi és üzemeltetői hátterét.</strong> Segítségével tájékozódhatsz, kinél cseng a
+kassza a költésed nyomán, ha asztalt vagy szállást foglalsz.</p>
+<p>Az oldal tartalmát az alábbi módszertan alapján a <a href="https://k-monitor.hu/fooldal">K-Monitor készíti</a>. Az oldal fejlesztésben a <a href="https://github.com/Code-for-Hungary">Code for Hungary</a> önkéntesei vettek részt. Az eszköz <a href="https://github.com/Code-for-Hungary/nerhotel">nyílt forráskóddal</a> rendelkezik, így szabadon továbbfejleszthető, alakítható és felhasználható. Ha segítenél, <a href="https://docs.google.com/forms/d/e/1FAIpQLSeep6bUaI0nC-ZelkPjdUdw_kvzAJu2XJc8qpNhAJeIRSyZEA/viewform">itt tudsz beszállni a csapatba</a>!</p>
+</section>
+<section>
+<h2>Milyen helyek szerepelnek rajta?</h2>
+<p>A NER HOTEL célja a szállás-, vendéglátó- és idegenforgalmihelyek tulajdonosi hátterének átláthatóbbá tétele. A honlapon olyan helyekek találhatók, amelyek üzemeltetője</p>
+<ol>
+<li><strong>politikailag exponált személyekhez</strong> (PEP) köthető,</li>
+<li><strong>irányításában PEP-ek vesznek részt</strong>, vagy</li>
+<li>tulajdonosi hátterében politikaközeli szereplőkhöz köthető (<a href="https://g7.hu/vallalat/20220214/a-meszaros-fele-alapokat-gazdagitja-a-magyar-cegek-profitjanak-6-szazaleka/">a tulajdonosok elrejtésére alkalmas</a>) <strong>magántőkealap</strong> szerepel.</li>
+</ol>
+<p>Előfordulhat, hogy egy ingatlannak más a tulajdonosa, mint az ott üzemelő helynek, ilyenkor az utóbbit vesszük alapul, mivel a fogyasztásból
+eredő haszon közvetlenül az üzemeltetőnél csapódik le. Politikailag exponált személynek elsősorban a <a href="https://net.jogtar.hu/jogszabaly?docid=a1700053.tv">2017. évi LIII. törvény</a> 4. §-ában adott meghatározást követve a kiemelt
+közszereplőket, ezek közeli hozzátartozóit és a velük közeli kapcsolatban álló személyeket tekintjük. Ez a definíció a nemzetközi pénzügyi visszaélésekkel összefüggésben, kockázati elven
+határozza meg a PEP-ek egy bizonyos körét. A hazai idegenforgalom politikailag exponált szereplőinek azonosítása érdekében a NER HOTEL a nemzetközi átvilágítási sztenderdekre is
+tekintettel egy ennél bővebb definíciót használ. Ez tartalmazza az önkormányzati döntéshozókat, a turisztikai ágazat kormányzati szervezeteinek vezetőit, illetve azokat a személyeket, akik
+sajtóprofiljuk alapján valamely politikailag exponált személlyel kiterjedt, a szereplők reputációját befolyásoló kapcsolatot tartanak fenn.</p>
+<p>A NER HOTEL célja, hogy ezen személyek turisztikai érdekeltségeiről hiteles és rendszerezett információt kínáljon, ami egy szempontja lehet a tudatos fogyasztói döntéseknek. Az ehhez
+szükséges információk feltérképezését nyilvánosan is elérhető sajtóhírek és cégadatok alapján, a <a href="https://adatbazis.k-monitor.hu/">K-Monitor sajtóadatbázisát</a> felhasználva folyamatosan végezzük.</p>
+<p>Az aktuális adatbázis tartalmának csv formátumban letöltéséhez <a href="/data-export">kattints ide</a>.</p>
+</section>
+<section>
+<h2>Milyen adatokra alapozzuk mindezt?</h2>
+<p>Az oldalon feltüntetett, politikai érintettséget vagy gazdasági érdekeltséget alátámasztó, a köz érdeklődésére számot tartó információk bárki számára hozzáférhetőek és ellenőrizhetőek. Az
+adatbázisban szereplő helyek tulajdonosainak vagy üzemeltetőinek politikai szimpátiája nem szolgálhat alapul a NER Hotelen történő megjelenésre.</p>
+<p><strong>Személyek háttere:</strong> <a href="https://k-monitor.hu/adatbazis">K-Monitor sajtóadatbázis</a></p>
+<p><strong>Céginformációk:</strong> Bisnode</p>
+<p><strong>Helyek adatai:</strong> Országos Kereskedelmi Nyilvántartás, önkéntes gyűjtés (helyek honlapjai, sajtócikkek, <a href="https://k-monitor.hu/adatbazis/cimkek/kisfaludy-program">Kisfaludy Program</a>, stb.)</p>
+</section>
+<section>
+<h2>Milyen forrásból? Honnan volt rá pénz?</h2>
+<p>Az ötlet első publikus megfogalmazását jelentő 2018-ban írt <a href="https://k.blog.hu/2018/08/03/ner_hotel">blogposztunk</a> felhívására <strong>116 támogatónk adott össze összesen 577.213 forintot</strong>. Ebből az összegből részben a fejlesztés során felmerülő, valamint a fenntartáshoz szükséges elkerülhetetlen költségeket fedeztük, részben kommunikációra költöttünk.</p>
+<p>Ezen mikroadományokon túl más forrás nem kapcsolódik a projekthez.</p>
+<p><strong>Ha értékesnek találod a munkánkat, <a href="https://tamogatas.k-monitor.hu">támogasd a K-Monitort rendszeres adományoddal</a>!</strong></p>
+</section>
+<section>
+<h2>Hibát találtál? Jelezd felénk!</h2>
+<p>Az idegenforgalom és vendéglátás folyamatos mozgásban van, gyakran történnek változások a céges adatokban. Elképzelhető, hogy bizonyos, a NER Hotelben feltüntetett adatok mára elavultak, nem pontosak vagy éppen hiányosak.</p>
+<p>Büszkék vagyunk rá, hogy önkéntesek segítségével építettük az alkalmazást és nyitottak vagyunk minden észrevételre. Járulj hozzá te is a fejlesztéshez! <a href="info@k-monitor.hu">Írd meg nekünk</a>, ha hibát találsz, vagy ha egy hely megjelenítésével nem értesz egyet! A beküldött információkat természetesen nem közvetlenül, hanem csak a saját módszertanunkkal való összevetés után vezetjük fel.</p>
+</section>
+`;export{e as attributes,t as html};
---
+++ dist/assets/contact-8009bf2e.js
@@ -0,0 +1,24 @@
+const t={},e=`<section>
+<h1>K-Monitor</h1>
+<p>A K-Monitor Iroda 2007 óta küzd a közpénzek átlátható
+felhasználásáért és a korrupció visszaszorításáért. Civil
+szervezetünk olyan politikai, gazdasági és társadalmi környezetet
+megteremtéséért dolgozik, ahol az állampolgárok elutasítják, a
+hatóságok pedig hatékonyan feltárják és szankcionálják a korrupciót.</p>
+<p><a href="https://tamogatas.k-monitor.hu/">Támogasd munkánkat rendszeres adományoddal</a>!</p>
+</section>
+<section>
+<h2>Hely beküldése</h2>
+<p>Tudsz egy étteremről, szálláshelyről vagy egyéb turisztikai
+érdekeltségről, aminek az üzemeltetője vélhetően politikaközeli
+szereplő? Küldd el nekünk az alábbi <a href="https://www.partimap.eu/p/nerhotel-bekuldes">űrlapon</a> keresztül.</p>
+<p>Minden beküldést átnézünk, és a <a href="/about">módszertannak</a> megfelelés esetén hozzáadjuk az adatbázishoz.</p>
+</section>
+<section>
+<h2>Elérhetőség</h2>
+<p><strong>K-Monitor Közhasznú Egyesület</strong></p>
+<p>Levelezési cím: 1077 Budapest, Jósika u. 25. <br/>
+<a href="mailto:info@k-monitor.hu">info@k-monitor.hu</a><br/>
+<a href="https://k-monitor.hu">k-monitor.hu</a></p>
+</section>
+`;export{t as attributes,e as html};
