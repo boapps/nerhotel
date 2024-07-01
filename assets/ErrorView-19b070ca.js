7a9345d94a9f10b4a2c14c4ca1739a75b6c7d20a
boa <boa@protonmail.com>
2024-07-01 16:19:31 +0200

    Updates

---
+++ assets/ErrorView-19b070ca.js
@@ -0,0 +1,1 @@
+import{j as r,E as t}from"./index-8fdc0b70.js";import{L as e}from"./Layout-18db2b5e.js";function a(o){return r(e,{history:o.history,children:r(t,{})})}export{a as default};
--- assets/ErrorView-42f35d12.js
+++
@@ -1,1 +0,0 @@
-import{j as r,E as t}from"./index-a33e6449.js";import{L as e}from"./Layout-21019ad7.js";function a(o){return r(e,{history:o.history,children:r(t,{})})}export{a as default};
