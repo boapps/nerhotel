711025b22f431ea9cb0dd8eb7993a5bddf376dd2
boa <boa@protonmail.com>
2024-07-01 14:46:55 +0200

    Updates

---
+++ assets/ErrorView-11b136a2.js
@@ -0,0 +1,1 @@
+import{j as r,E as t}from"./index-f9495290.js";import{L as e}from"./Layout-c619d6fa.js";function a(o){return r(e,{history:o.history,children:r(t,{})})}export{a as default};
---
+++ assets/about-379db50d.js
@@ -0,0 +1,37 @@
+const e={},t=`<section>
+<h1>What is Hotel Oligarch?</h1>
+<p>Hungarian public discourse has long been characterised by the aspiration of the political elite to create an aligned class. With the right connections and political backing, it is possible to acquire considerable wealth in just a few years through targeted public procurements, non-repayable grants and soft loans. Today, in the tourism sector, the businesses that benefit from these processes have become unavoidable, with hundreds of hotels and restaurants being taken over by politically connected actors. As customers we still have the opportunity to vote with our wallets against the further expansion of the politically backed entrepreneurial class.</p>
+<p>The Hungarian name of the site (NER Hotel) refers to the System of National Cooperation (NER) introduced by the governing party that has led Hungary since 2010.</p>
+<p><strong>Hotel Oligarch is a web-based application that makes the ownership and management of Hungarian tourism establishments more transparent. It helps you to find out who profits when you book a table in a restaurant or stay in a hotel.</strong></p>
+<p>The content of the site is produced by <a href="https://k-monitor.hu/home">K-Monitor</a> using the following methodology. The site was developed by <a href="https://code4.hu/">Code for Hungary</a> volunteers. The tool is <a href="https://github.com/Code-for-Hungary/nerhotel">open source</a>, so it can be freely developed, adapted and used.</p>
+</section>
+<section>
+<h2>What kind of places are presented?</h2>
+<p>Hotel Oligarch includes hospitality, accommodation and tourism establishments that are operated by</p>
+<ol>
+<li><strong>owned by</strong> (in a whole or in part) <strong>politically exposed persons</strong> (PEPs),</li>
+<li><strong>managed by PEPs</strong>,</li>
+<li>an entity that is owned by a <strong>private equity fund</strong> linked to PEPs (with the ability to hide the beneficial owners).</li>
+</ol>
+<p>It may happen that the owner of a property is different from the owner of the operating company, in which case the latter is taken as the basis, since the profits accrue directly to the operator. Politically exposed persons are primarily considered to be prominent public figures, their close relatives and persons closely associated with them, as defined in <em>Article 4 of Act LIII of 2017</em>. In order to identify PEPs in the tourism industry, Hotel Oligarch - in line with international auditing standards - uses a broader definition: this includes decision-makers in local governments, heads of governmental organizations in the tourism sector, and persons who, based on their press profile, have extensive links with a politically exposed person.</p>
+<p>Hotel Oligarch aims to provide credible and structured information on the tourism interests of such people, and serve as a guide in making informed consumer choices.</p>
+<p>To download the current database in csv format, <a href="/data-export">click here</a>.</p>
+</section>
+<section>
+<h2>What's the data behind it all?</h2>
+<p>All the information available on the website, which may demonstrate political ties or economic interests, is accessible and verifiable by anyone from public sources. The political views of the owners or operators of the establishments listed in the database should not be used as a basis for inclusion on the NER Hotel.</p>
+<p><strong>Company information</strong>: <a href="https://partnercontrol.hu">Bisnode</a></p>
+<p><strong>Background of individuals</strong>: <a href="https://adatbazis.k-monitor.hu">K-Monitor press database</a></p>
+<p><strong>Data about the establishments</strong>: National Trade Register, user submissions, collection by our volunteers (websites of places, press articles, Kisfaludy Programme, etc.).</p>
+</section>
+<section>
+<h2>What was the financial resource for the website?</h2>
+<p>The first public <a href="https://k.blog.hu/2018/08/30/hotel_oligarch">formulation of the idea</a> of the project was in 2018, which raised a total of <strong>HUF 577,213 (~ EUR 1800) from 116 donors</strong>. This amount was partly used to cover the development and maintenance costs, and partly for communication.</p>
+<p>Only micro-donations were used to finance the project.</p>
+<p><strong><a href="https://k-monitor.hu/support">Choose a regular donation plan</a> for K-Monitor if you find our work valuable!</strong></p>
+</section>
+<section>
+<h2>Found a mistake? Report it!</h2>
+<p>The tourism industry is constantly in flux, with frequent changes in company data. It may happen that some of the information on Hotel Oligarch is outdated, inaccurate or incomplete.</p>
+<p>Contribute to the development! We are proud to have built the website with the help of volunteers and user submissions. We are also open to any comments. <a href="info@k-monitor.hu">Let us know</a> if you find a bug or disagree with the way an establishment is displayed! Of course, the information you submit will not be listed directly, only after checking it against our own methodology.</p>
+</section>`;export{e as attributes,t as html};
